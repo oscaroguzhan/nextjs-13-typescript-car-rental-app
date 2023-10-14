9a9d399fddf6e3e9a257bf9408aac89327798cd8
oscarwergun <127099022+oscarwergun@users.noreply.github.com>
2023-10-14 22:19:19 +0200

    completed layout section

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import Image from "next/image"
+import  CustomButton  from "./CustomButton"
+const Navbar = () => {
+  return (
+    <header className="w-full sticky top-0 scroll-smooth z-20 bg-[#fafafa]">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center py-4 px-6 sm:px-16">
+        <Link href="https://codeguruozzy.com" className="flex justify-center items-center">
+          <Image src="/ozzylogo.png" alt="nav-logo" width={120} height={20} className="object-contain"/>
+        </Link>
+        <div className="flex">
+        <CustomButton 
+        title='Sign in'
+        containerStyle=' underline pointer text-[var(--white)] bg-black outline-none rounded-full hover:bg-[var(--purple)]'
+        btnType="button"
+        />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
